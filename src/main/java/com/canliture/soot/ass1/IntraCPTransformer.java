1a6a0230503c490e043ea298db4be6193baf24da
canliture <canliture@outlook.com>
2021-09-19 18:21:33 +0800

    自定义BodyTransformer用于实现常量传播

---
+++ src/main/java/com/canliture/soot/ass1/IntraCPTransformer.java
@@ -0,0 +1,28 @@
+package com.canliture.soot.ass1;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.Unit;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+
+import java.util.Map;
+
+/**
+ * Created by liture on 2021/9/19 4:46 下午
+ */
+public class IntraCPTransformer extends BodyTransformer {
+
+    @Override
+    protected void internalTransform(Body b, String phaseName, Map<String, String> options) {
+        UnitGraph unitGraph = new BriefUnitGraph(b);
+        IntraConstantPropagation propagation = new IntraConstantPropagation(unitGraph);
+        propagation.doAnalysis();
+        System.out.println(String.format("=========== Method %s ============\n", b.getMethod().getName()));
+        for (Unit unit : b.getUnits()) {
+            System.out.println(String.format("Before %s: %s", unit, propagation.getFlowBefore(unit)));
+            System.out.println(String.format("After %s: %s", unit, propagation.getFlowAfter(unit)));
+            System.out.println("==================================================================\n");
+        }
+    }
+}
