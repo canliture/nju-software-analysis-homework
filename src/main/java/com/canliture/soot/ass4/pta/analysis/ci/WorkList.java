927ed9a6b70cdd50b5b0893dc7210cd26ffc7e6e
canliture <canliture@outlook.com>
2021-09-21 13:55:53 +0800

    更新worklist注释，为其添加两个API

--- src/main/java/com/canliture/soot/ass4/pta/analysis/ci/WorkList.java
+++ src/main/java/com/canliture/soot/ass4/pta/analysis/ci/WorkList.java
@@ -1,5 +1,9 @@
 package com.canliture.soot.ass4.pta.analysis.ci;
 
+import soot.toolkits.scalar.Pair;
+
+import java.awt.*;
+
 /**
  * Created by liture on 2021/9/20 11:11 下午
  *
@@ -8,11 +12,27 @@ package com.canliture.soot.ass4.pta.analysis.ci;
 public class WorkList {
 
     /**
-     * 添加一条 <p, pts(p)> 到workList
-     * @param p
-     * @param pts
+     * 添加一条 <p, pts> 到workList
+     * @param p 指针
+     * @param pts 一个指向集合，它所表示的对象被传播到这个指针的pts
      */
     public void addPointerEntry(Pointer p, PointsToSet pts) {
         // todo
+    }
+
+    /**
+     * @return workList是否为空
+     */
+    public boolean isEmpty() {
+        // todo
+        return true;
+    }
+
+    /**
+     * @return 从workList取出一条 <p, pts>
+     */
+    public Pair<Pointer, PointsToSet> remove() {
+        // todo
+        return null;
     }
 }
