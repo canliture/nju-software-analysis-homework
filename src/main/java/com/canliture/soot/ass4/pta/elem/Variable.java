e9a6fc712328f611858daa2b74f5ab9deca54675
canliture <canliture@outlook.com>
2021-09-25 19:51:11 +0800

    添加Variable相关的所在的Method的API

--- src/main/java/com/canliture/soot/ass4/pta/elem/Variable.java
+++ src/main/java/com/canliture/soot/ass4/pta/elem/Variable.java
@@ -49,6 +49,10 @@ public class Variable {
         return calls;
     }
 
+    public Method getMethod() {
+        return method;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
