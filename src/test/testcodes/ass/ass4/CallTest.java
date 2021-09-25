9d6859d123760b1c6fe81d12e96984e8e2df2149
canliture <canliture@outlook.com>
2021-09-25 19:49:14 +0800

    添加包含调用的指针分析测试代码

---
+++ src/test/testcodes/ass/ass4/CallTest.java
@@ -0,0 +1,30 @@
+package ass4;
+
+/**
+ * Created by liture on 2021/9/22 8:54 下午
+ */
+public class CallTest {
+
+    static void main() {
+        A.main();
+    }
+}
+
+class A {
+    static void main() {
+        A a = new A();
+        A b = new B();
+        A c = b.foo(a);
+    }
+
+    A foo(A x) {
+        return x;
+    }
+}
+
+class B extends A {
+    A foo(A y) {
+        A r = new A();
+        return r;
+    }
+}
